25e17b9ca856d2e2a82a10fb6449edcc77813581
Bob Jarvis <bobtheclawjarvis@gmail.com>
2026-02-03 21:50:39 -0300

    Redeployed website files

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Smooth scrolling for navigation links
     const navLinks = document.querySelectorAll('nav ul li a');
-
     navLinks.forEach(link => {
         link.addEventListener('click', (e) => {
             e.preventDefault();
